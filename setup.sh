00dc2266583a7776bc12562f2359e7f82fadde43
Hamza <hhamza@student.1337.ma>
2022-05-17 15:28:11 +0100

    Fix setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 INSTALL_PATH="$HOME/Applications/bin"
 
 mkdir -p $INSTALL_PATH
-mv -f normcolor.sh $INSTALL_PATH
-mv -f remove.sh $INSTALL_PATH
-echo 'alias norminette="$INSTALL_PATH/normcolor.sh"' >> ~/.zshrc
+cp -f normcolor.sh $INSTALL_PATH
+cp -f remove.sh $INSTALL_PATH
+echo "alias norminette=\"$INSTALL_PATH/normcolor.sh\"" >> ~/.zshrc
 exec zsh
